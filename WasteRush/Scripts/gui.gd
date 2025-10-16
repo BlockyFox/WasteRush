7d39cdf51a9e404971c41f2789170a3c8ae7cd57
Blocks <130404463+BlockyFox@users.noreply.github.com>
2025-10-16 19:03:57 -0400

    Delete WasteRush directory

--- WasteRush/Scripts/gui.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer
-
-
-
-
-func _process(_delta: float) -> void:
-	$Sprint_Bar.value = Globals.SprintBar
-	$HealthBar.value = Globals.PlayerHealth
-	
--- WasteRush/Scripts/texture_button.gd
+++
@@ -1,5 +0,0 @@
-extends TextureButton
-
-func _process(delta: float) -> void:
-	if button_pressed:
-		get_tree().change_scene_to_file("res://scenes/stage_1.tscn")
